3cfb22914453f22e255fa5f7458ec5862400fde0
hamza5999 <mhamzamahmood5@gmail.com>
2023-11-17 15:05:42 +0500

    Add tap listener to category grid items

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ The 'Meals App' is a Flutter project that allows users to browse meal categories
 
 - GridView
 - SliverGridDelegateWithFixedCrossAxisCount
+- InkWell
 
 ## Things learned:
 
 - Made a grid using GridView widget to display categories in a grid
 - Passed a default value to to a parameter in case no value given to that parameter
+- Used InkWell widget to make any widget tapable and to add a highlighting effect on clicking that item
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -8,24 +8,31 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    // Used InkWell to add tap gesture and a tapping affect to the widget
+    return InkWell(
+      onTap: () {},
+      // this borderRadius is for the tapping affect
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          // this borderRadius is for the grid item container
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       ),
     );
   }
